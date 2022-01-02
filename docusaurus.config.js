a4096e60db69e3353bde26ac4dd1e0797948cbfa
Vasili Cekaskin <vasili.cekaskin@invision.de>
2022-01-02 21:01:53 +0100

    Change base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "Buchhaltung",
   tagline: "Dinosaurs are cool",
   url: "https://buchhaltung.github.io",
-  baseUrl: "/",
+  baseUrl: "/Buchhaltung/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
