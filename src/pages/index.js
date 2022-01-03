87bb2ff376b376d626649cc4f1f3fefcb4dc4d02
Lisa Rothe <liskari@Lisas-MacBook-Pro.local>
2022-01-03 11:59:59 +0100

    rename hero subtitle

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Eine Erklärung
+            Eine Erklärung von Lisa Rothe
           </Link>
         </div>
       </div>
